75cd53af9cea4188162b89c2a40adc2a261eb223
APonce27 <aponceg@ucenfotec.ac.cr>
2019-03-18 20:40:33 -0600

    arreglo de dropdown de distritos

--- Public/js/controlador_ubicacion.js
+++ Public/js/controlador_ubicacion.js
@@ -21,7 +21,7 @@ let llenar_cantones = () =>{
     for(let i = 0; i < cantones.length; i++){
         if(provincia == cantones[i]['Provincia_idProvincia']){
             let nuevaOpcion = new Option(cantones[i]['nombre']);
-            nuevaOpcion.value = cantones[i]['nombre'];
+            nuevaOpcion.value = cantones[i]['idCanton'];
             select_cantones.appendChild(nuevaOpcion);
         }
         
@@ -33,7 +33,7 @@ let llenar_distritos = () =>{
     select_distritos.innerHTML = '';
     
     for(let i = 0; i < distritos.length; i++){
-        if(cantones == distritos[i]['Cantones_idCanton']){
+        if(cantones == distritos[i]['Canton_idCanton']){
             let nuevaOpcion = new Option(distritos[i]['nombre']);
             nuevaOpcion.value = distritos[i]['nombre'];
             select_distritos.appendChild(nuevaOpcion);
@@ -46,3 +46,4 @@ select_provincias.addEventListener('change', llenar_cantones);
 select_cantones.addEventListener('change', llenar_distritos);
 
 llenar_provincias();
+
