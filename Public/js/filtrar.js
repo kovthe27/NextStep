799e2ea4a907be86daf635251876ce38a9c2e35d
kovthe27 <39032531+kovthe27@users.noreply.github.com>
2019-03-20 18:36:28 -0600

    Filter

--- Public/js/filtrar.js
+++ Public/js/filtrar.js
@@ -1,8 +1,13 @@
 'use strict';
 
+// Listas
 document.querySelector('#txt_search').onkeyup = function(){
     filtrarTabla('#tabla', this.value);
 }
+
+// utiles_Administrador
+document.querySelector('#txt_search').onkeyup(filtrarTabla('#TblUtiles', this.value));
+
 
 let filtrarTabla = (id, value) =>{
     var rows = document.querySelectorAll(id + ' tbody tr');
