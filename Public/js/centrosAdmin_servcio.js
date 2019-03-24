a37bf5449a963dc679f65cda40faa09643c8cebd
Javier <jmendez@ucenfotec.ac.cr>
2019-03-23 20:25:06 -0600

    Etiquetas Complete

---
+++ Public/js/centrosAdmin_servcio.js
@@ -0,0 +1,58 @@
+let consultar_etiquetasAdmin = () =>{
+    let lista_utilesAdmin = [];
+
+    let request = $.ajax({
+      url: "http://localhost:4000/api/consultar_etiqueta",
+      method: "GET",
+      data: {
+      },
+      dataType: "json",
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      async: false
+    });
+  
+    request.done(function (res) {
+        lista_utilesAdmin = res;
+      console.log("success");
+  
+    });
+  
+    request.fail(function (jqXHR, textStatus) {
+      console.log("fail");
+    });
+  
+    return lista_utilesAdmin;
+};
+
+let registrarEtiqueta = (pnombre, pfecha, pusuarios) =>{
+    let request = $.ajax({
+        url: "http://localhost:4000/api/registrar_etiqueta",
+        method: "POST",
+        data: {
+          nombre : pnombre,
+          fecha: pfecha,
+          usuarios: pusuarios
+        },
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+      });
+    
+      request.done(function (msg) {
+        swal.fire({
+          type: 'success',
+          title: 'La etiqueta ha sido agregada',
+          text: 'Muchas gracias'
+        });
+        bitacora("MEPAdmin1", "Registro", "Se agrego la etiqueta: "+pnombre);
+      });
+    
+      request.fail(function (jqXHR, textStatus) {
+        swal.fire({
+          type: 'error',
+          title: 'La etiqueta no pudo ser agregada',
+          text: 'Por favor inténtelo de nuevo'
+        });
+      });
+      return true;
+
+}
