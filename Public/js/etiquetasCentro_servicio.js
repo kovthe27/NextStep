ab8d4c4c4de95d57c81cd15df0e75004a238a3b8
Javier <jmendez@ucenfotec.ac.cr>
2019-03-24 17:36:30 -0600

    ListarCentros

--- Public/js/etiquetasCentro_servicio.js
+++ Public/js/etiquetasCentro_servicio.js
@@ -2,62 +2,62 @@
 'use strict';
 
 let registrar_etiquetasCentro = (pcedulajuridica, petiqueta1, petiqueta2, petiqueta3, petiqueta4) => {
-    let request = $.ajax({
-      url: "http://localhost:4000/api/registrar_etiquetasCentro",
-      method: "POST",
-      data: {
-        cedulaJuridica : pcedulajuridica,  
-        etiqueta1 : petiqueta1,
-        etiqueta2 : petiqueta2,
-        etiqueta3 : petiqueta3,
-        etiqueta4 : petiqueta4,
-      },
-      dataType: "json",
-      contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+  let request = $.ajax({
+    url: "http://localhost:4000/api/registrar_etiquetasCentro",
+    method: "POST",
+    data: {
+      cedulaJuridica: pcedulajuridica,
+      etiqueta1: petiqueta1,
+      etiqueta2: petiqueta2,
+      etiqueta3: petiqueta3,
+      etiqueta4: petiqueta4,
+    },
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+  });
+
+  request.done(function (msg) {
+    swal.fire({
+      type: 'success',
+      title: 'Las etiquetas fueron agregadas',
+      text: 'Muchas gracias'
     });
-  
-    request.done(function (msg) {
-      swal.fire({
-        type: 'success',
-        title: 'Las etiquetas fueron agregadas',
-        text: 'Muchas gracias'
-      });
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    swal.fire({
+      type: 'error',
+      title: 'Las etiquetas no fueron guardadas',
+      text: 'Por favor inténtelo de nuevo'
     });
-  
-    request.fail(function (jqXHR, textStatus) {
-      swal.fire({
-        type: 'error',
-        title: 'Las etiquetas no fueron guardadas',
-        text: 'Por favor inténtelo de nuevo'
-      });
-    });
-  };
-  
+  });
+};
 
 
-let consultar_etiquetasCentro= () => {
-    let lista_etiquetasCentro = []; 
-    
-      let request = $.ajax({
-        url: "http://localhost:4000/api/consultar_etiquetasCentro",
-        method: "GET",
-        data: {
-        },
-        dataType: "json",
-        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-        async : false
-      });
-    
-      request.done(function (res) {
-        lista_etiquetasCentro = res;
-        // console.log("success");
-        
-      });
-    
-      request.fail(function (jqXHR, textStatus) {
-        // console.log("fail");
-      });
-    
-      return lista_etiquetasCentro;
-      
-    };
+
+let consultar_etiquetasCentro = () => {
+  let lista_etiquetasCentro = [];
+
+  let request = $.ajax({
+    url: "http://localhost:4000/api/consultar_etiquetasCentro",
+    method: "GET",
+    data: {
+    },
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    async: false
+  });
+
+  request.done(function (res) {
+    lista_etiquetasCentro = res;
+    // console.log("success");
+
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    // console.log("fail");
+  });
+
+  return lista_etiquetasCentro;
+
+};
