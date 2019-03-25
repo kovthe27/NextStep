e22a5228d0df3f80fc17dade7ad6b046b70ce60c
Javier <jmendez@ucenfotec.ac.cr>
2019-03-25 13:17:16 -0600

    Login-Carga perfil

--- Public/js/login_servicio.js
+++ Public/js/login_servicio.js
@@ -4,11 +4,21 @@ let ubicacionUsuario;
 let verificarUsuario = (pUsuario, pContrasena) => {
     let success = false;
     let usuarios = consultar_listaUsuario();
+    let centros = consultar_listaCentros();
 
     for (let i = 0; i < usuarios.length; i++) {
         if (usuarios[i].emailUsuario == pUsuario) {
             if (usuarios[i].contrasenaUsuario == pContrasena) {
                 success = true;
+            }
+        }
+    }
+    if(success == false){
+        for (let i = 0; i < centros.length; i++) {
+            if (centros[i].emailCentro == pUsuario) {
+                if (centros[i].contrasenaCentro == pContrasena) {
+                    success = true;
+                }
             }
         }
     }
@@ -41,8 +51,35 @@ let consultar_listaUsuario = () => {
     return lista_usuarios;
 }
 
+let consultar_listaCentros = () => {
+    let lista_usuarios = [];
+
+    let request = $.ajax({
+        url: "http://localhost:4000/api/consultar_centro",
+        method: "GET",
+        data: {
+        },
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        async: false
+    });
+
+    request.done(function (res) {
+        lista_usuarios = res;
+        console.log("success");
+
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        console.log("fail");
+    });
+
+    return lista_usuarios;
+}
+
 let cargarUsuario = (pUsuario) => {
     let usuarios = consultar_listaUsuario();
+    let centros = consultar_listaCentros();
     let tipo;
 
 
@@ -52,17 +89,15 @@ let cargarUsuario = (pUsuario) => {
         }
     }
     if (tipo == "Padre") {
-        bitacora(pUsuario, "Inicio de sesion", "Sistema operativo: "+ navigator.platform);
+        bitacora(pUsuario, "Inicio de sesion", "Sistema operativo: " + navigator.platform);
         window.location.assign("principal_padres.html");
     } else {
         if (tipo == "Administrador") {
-            bitacora(pUsuario, "Inicio de sesion", "Sistema operativo: "+ navigator.platform);
+            bitacora(pUsuario, "Inicio de sesion", "Sistema operativo: " + navigator.platform);
             window.location.assign("principal_administrador.html");
         } else {
-            if (tipo == "Centro") {
-                bitacora(pUsuario, "Inicio de sesion", "Sistema operativo: "+ navigator.platform);
-                window.location.assign("principal_CentroEducativo.html");
-            }
+            bitacora(pUsuario, "Inicio de sesion", "Sistema operativo: " + navigator.platform);
+            window.location.assign("principal_CentroEducativo.html");
         }
     }
 
