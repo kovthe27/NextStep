6bfcdffdfc64524b8c153637e7f6349bed37d447
ebarquero <ebarquero@altus.co.cr>
2019-04-12 17:07:05 -0600

    eliminar-editarPreguntas

--- Public/js/preguntasAdmin_servicio.js
+++ Public/js/preguntasAdmin_servicio.js
@@ -61,3 +61,103 @@ let registrarPregunta = (pNumero, pPregunta, pRespuesta) => {
   });
   return true;
 }
+
+
+
+let buscar_pregunta = (p_id) => {
+  let pregunta = [];
+
+  let request = $.ajax({
+    url: "http://localhost:4000/api/buscar_pregunta",
+    method: "POST",
+    data: {
+      id_pregunta: p_id
+    },
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    async : false
+  });
+
+  request.done(function (res) {
+    pregunta = res;
+    console.log("success");
+    
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    console.log("fail");
+  });
+
+return pregunta;
+   
+}
+
+
+let actualizar_pregunta = (ppregunta, prespuesta, p_id) =>{
+  let request = $.ajax({
+      url : 'http://localhost:4000/api/actualizar_pregunta',
+      method : "POST",
+      data : {
+        pregunta: ppregunta,
+        respuesta: prespuesta,
+        id_pregunta: p_id
+      },
+      dataType : "json",
+      contentType : 'application/x-www-form-urlencoded; charset=UTF-8' 
+  });
+
+  request.done(function(res){
+      swal.fire({
+          type : 'success',
+          title : 'Proceso realizado con éxito',
+          text : res.msg
+      });
+
+  });
+
+  request.fail(function(res){
+      swal.fire({
+          type : 'error',
+          title : 'Proceso no realizado',
+          text : res.msg
+      });
+
+  });
+
+};
+
+let eliminar_pregunta = (p_id) => {
+let request = $.ajax({
+  url : 'http://localhost:4000/api/eliminar_pregunta',
+  method : "POST",
+  data : {
+    id_pregunta: p_id
+  },
+  dataType : "json",
+  contentType : 'application/x-www-form-urlencoded; charset=UTF-8' 
+});
+
+request.done(function(res){
+  swal({
+      type : 'success',
+      title : 'Proceso realizado con éxito',
+      text : res.msg
+  });
+
+  document.querySelector('#actualizarpregunta').innerHTML= "";
+  window.location.reload();
+
+});
+
+request.fail(function(res){
+  swal({
+      type : 'error',
+      title : 'Proceso no realizado',
+      text : res.msg
+  });
+
+});
+
+};
+
+
